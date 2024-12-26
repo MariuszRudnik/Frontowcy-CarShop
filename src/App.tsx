02055e50d279bdc9afef0b608318a6b5fc3c0264
mrjson <mariuszjson@gmail.com>
2024-12-26 20:29:10 +0100

    initial project

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import './App.css'
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import React from 'react'
+
+function App() {
+
+
+  return (
+    <React.StrictMode>
+      <p>ss</p>
+    </React.StrictMode>
+  )
+}
+
+export default App
