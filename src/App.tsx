8fd1dcd76171d8822c8ec1916d75a6da59d7282d
mrjson <mariuszjson@gmail.com>
2025-01-07 22:23:59 +0100

    Add reset method to store and new components for order process

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
-import { RouterProvider, createRouter } from '@tanstack/react-router';
 import { routeTree } from './routeTree.gen';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { createRouter, RouterProvider } from '@tanstack/react-router';
 
-const router = createRouter({ routeTree });
+const router = createRouter({
+  routeTree,
+  context: { queryClient: undefined! },
+});
 
 declare module '@tanstack/react-router' {
   interface Register {
@@ -10,5 +14,10 @@ declare module '@tanstack/react-router' {
 }
 
 export const App = () => {
-  return <RouterProvider router={router} />;
+  const queryClient = new QueryClient();
+  return (
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} context={{ queryClient }} />
+    </QueryClientProvider>
+  );
 };
